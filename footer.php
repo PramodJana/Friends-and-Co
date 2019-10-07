cc0074c8f5ff399f959bb904d231b7ddd0865d81
shashankmishra998 <46535650+shashankmishra998@users.noreply.github.com>
2019-10-07 21:26:00 +0530

    Add files via upload
    new footer commit

--- footer.php
+++ footer.php
@@ -5,21 +5,47 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
   </head>
   <body>
       <?php
+
+
+     
       echo '<footer class="page-footer font-small blue pt-4" style="background-color:#004D40;color:white">
 
     <!-- Footer Links -->
     <div class="container-fluid text-center text-md-left">
 
           <!-- Footer Links -->
+          <div class="container-icon">
+          <h3>Let\'s give a depth in our relationship and connect with us through...</h3><hr>
+       <div class="row">
+           <div class="col-sm">
+              <a href="https://www.youtube.com/watch?v=P-DhwN87JDY"> <img src="icon/fb.png" style="width:64px"></a>
+           </div>
+           <div class="col-sm">
+                <img src="icon/insta.png" style="width:64px">
+            </div>
+            <div class="col-sm">
+                    <img src="icon/wapp.png" style="width:64px">
+                </div>
+                <div class="col-sm">
+                        <img src="icon/twit.png" style="width:64px">
+                    </div>
+                    <div class="col-sm">
+                            <img src="icon/help.png" style="width:64px">
+                        </div>
+
+       </div>
+   </div>
+
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2019 Copyright:
        Friends And Co.
     <br>
-                Designed By : Pramod Jana ...!!!
+                Designed By : Pramod Jana and Shashank Mishra!!!
                 </div>
     <!-- Copyright -->
 
